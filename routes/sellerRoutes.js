0730455f48f22c031e2aadf710352cfb70e7f170
rajat-k-27 <chakrabortyrajat3689@gmail.com>
2025-03-15 01:28:22 +0530

    seller auth completed

---
+++ routes/sellerRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import multer from "multer";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import { saveSellerDetails } from "../controllers/sellerController.js";
+import { storage } from "../config/cloudinaryConfig.js";
+
+const upload = multer({ storage }); // Use Cloudinary storage
+
+const router = express.Router();
+
+router.put("/update", authMiddleware, upload.single("shopPhoto"), saveSellerDetails);
+
+export default router;
